2fcbdc6094a9dd5a6aff69f13846ac35168709e0
hasheemismath <hasheemhush@gmail.com>
2020-05-28 01:15:38 +0530

    adding payment component

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../styles.css";
-import { API } from "../backend";
 import Base from "./Base";
 import Card from "./Card";
 import { loadCart } from "./helper/cartHelper";
+import StripeCheckout from "./StripeCheckout";
 
 const Cart = () => {
   const [products, setProducts] = useState([]);
@@ -42,7 +42,9 @@ const Cart = () => {
     <Base title="Cart Page" description="Ready to checkout">
       <div className="row text-center">
         <div className="col-6">{loadAllProducts()}</div>
-        <div className="col-6">Checkout section</div>
+        <div className="col-6">
+          <StripeCheckout products={products} setReload={setReload} />
+        </div>
       </div>
     </Base>
   );
